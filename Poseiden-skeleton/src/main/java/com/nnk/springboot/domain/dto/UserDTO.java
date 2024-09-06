9191f9735626c5b1e57fa032eadb0ca04af19f74
yas43 <ykeshtdar@yahoo.com>
2024-09-06 09:47:20 +0200

    get stuck for adding authorization

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/dto/UserDTO.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/dto/UserDTO.java
@@ -9,10 +9,13 @@ import lombok.*;
 public class UserDTO {
     private Integer id;
     @NotBlank(message = "Username is mandatory")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{1,8}$" )
     private String username;
     @NotBlank(message = "FullName is mandatory")
     private String fullname;
     @NotBlank(message = "Password is mandatory")
+    @Pattern(regexp = "^\\d+$")
+    @Size(min = 8)
     private String password;
     @NotBlank(message = "Role is mandatory")
     private String role;
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/security/securityConfiguration.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/security/securityConfiguration.java
@@ -1,5 +1,7 @@
 package com.nnk.springboot.security;
 
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.*;
 import com.nnk.springboot.service.*;
 import jakarta.servlet.http.*;
 import org.springframework.context.annotation.*;
@@ -10,18 +12,29 @@ import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.configurers.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.authority.*;
+import org.springframework.security.core.authority.mapping.*;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.*;
+import org.springframework.security.oauth2.client.userinfo.*;
+import org.springframework.security.oauth2.core.oidc.user.*;
+import org.springframework.security.oauth2.core.user.*;
 import org.springframework.security.web.*;
+
+import java.util.*;
+import java.util.stream.*;
 
 @Configuration
 @EnableWebSecurity
 public class securityConfiguration {
     private final CustomUserDetailsService customUserDetailsService;
+    private final UserRepository userRepository;
 
-    public securityConfiguration(CustomUserDetailsService customUserDetailsService) {
+    public securityConfiguration(CustomUserDetailsService customUserDetailsService, UserRepository userRepository) {
         this.customUserDetailsService = customUserDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Bean
@@ -38,6 +51,8 @@ public class securityConfiguration {
 
                 .oauth2Login(httpSecurityOAuth2LoginConfigurer -> {
                     httpSecurityOAuth2LoginConfigurer
+                            .userInfoEndpoint(userInfo -> userInfo
+                                    .userService(oAuth2UserService()))
                             .defaultSuccessUrl("/bidList/list")
                             .failureUrl("/login?error=true");
                 })
@@ -72,6 +87,33 @@ public class securityConfiguration {
 
                 .build();
     }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        return userRequest -> {
+            OAuth2User oauth2User = (OAuth2User) userRequest.getAdditionalParameters().get("user");
+
+            String githubUsername = oauth2User.getAttribute("login"); // GitHub username
+
+            // Fetch the user from the database by GitHub login
+            User user = userRepository.findByUsername(githubUsername)
+                    .orElseThrow(() -> new RuntimeException("User not found in the database"));
+
+            // Convert the user's roles from the database to SimpleGrantedAuthority
+//            Set<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+//                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+//                    .collect(Collectors.toSet());
+
+            Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+            authorities.stream()
+                    .map(s -> new SimpleGrantedAuthority(user.getRole()))
+                    .collect(Collectors.toList());
+
+            // Return the authenticated OAuth2User with the roles fetched from the database
+            return new DefaultOAuth2User(authorities, oauth2User.getAttributes(), "login");
+        };
+    }
+
 
     @Bean
     public UserDetailsService userDetailsService(){
